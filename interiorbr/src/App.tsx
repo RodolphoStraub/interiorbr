44e1c4faf0c5b8d51928b6bdefa7562c6ddb6b64
RodolphoStraub <rodolphostraubcertari63@gmail.com>
2023-07-31 17:12:06 -0300

    header home about

---
+++ interiorbr/src/App.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Sobre from "./components/Sobre";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Sobre />
+    </div>
+  );
+}
+
+export default App;
